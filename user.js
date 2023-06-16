ae70fb3c2325ca52ed6095cb514cd7ea9e7eac52
justGyga <iwantdrink229@gmail.com>
2023-06-16 18:23:03 +0700

    add database

---
+++ user.js
@@ -0,0 +1,33 @@
+import DataTypes from 'sequelize';
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  secondName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  login: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  timestamps: false
+});
+
+module.exports = User;
+
